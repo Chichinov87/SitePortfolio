abdee2f83134f1aae45c8d0266b1277f909700ff
userAnatolio <89261969132@mail.ru>
2020-05-25 18:56:34 +0300

    Добавил класс для отправки сообщений на почту  EmailService.cs

---
+++ EmailService/EmailService.cs
@@ -0,0 +1,28 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using System.Threading.Tasks;
+
+namespace SytePortfolio.Service
+{
+    public class EmailService
+    {
+        public static async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress("Администрация сайта", "89261969132@mail.ru"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.mail.ru", 465, true);
+                await client.AuthenticateAsync("89261969132@mail.ru", ".toly52515tolik52515.");
+                await client.SendAsync(emailMessage);
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
